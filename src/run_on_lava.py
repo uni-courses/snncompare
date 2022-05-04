084f77f44ffd2fbe19d15dbc4fa01c3b69c5711a
name <some@email.com>
2022-05-04 15:18:19 +0200

    Wrote basic verification of nodes and edges.

--- src/run_on_lava.py
+++ src/run_on_lava.py
@@ -4,13 +4,9 @@
 # to an SNN network that can be ran by Lava, and retrieves a first/single
 # neuron. The simulation is than ran for t timesteps on a Loihi emulation.
 
-# Import the networkx module.
-import networkx as nx
 
-
-def verify_networkx_graph_is_valid_snn_specification(G: nx.Graph) -> bool:
-    for node in G.nodes:
-        node
+def simulate_network_on_lava():
+    pass
 
 
 def convert_networkx_graph_to_lava_snn():
--- src/verify_graph_is_snn.py
+++ src/verify_graph_is_snn.py
@@ -1,14 +1,22 @@
 # Verifies the graph represents a connected and valid SNN, with all required
 # neuron and synapse properties specified.
 
-
-def verify_networkx_graph():
-    pass
-
-
-def verify_neuron_properties_are_specified():
-    pass
+# Import the networkx module.
+import networkx as nx
 
 
-def verify_synapse_properties_are_specified():
-    pass
+def verify_networkx_graph_is_valid_snn_specification(G: nx.Graph) -> bool:
+    for node in G.nodes:
+        verify_neuron_properties_are_specified(node)
+
+
+def verify_neuron_properties_are_specified(node: nx.Graph.nodes):
+    assert isinstance(node["bias"], int), "Bias is not an integer."
+    assert isinstance(node["du"], int), "du is not an integer."
+    assert isinstance(node["dv"], int), "dv is not an integer."
+    assert isinstance(node["vth"], int), "vth is not an integer."
+
+
+def verify_synapse_properties_are_specified(edge):
+    assert isinstance(edge["w"], int), f"Weight of edge {edge} is not an"
+    +" integer."
