b1e6ba199c536ad0c7f048d03f9d5da972d2fde4
name <some@email.com>
2022-05-31 12:12:18 +0200

    Refactored random graph generation for tests.

---
+++ src/Scope_of_tests.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""File represents LIF neuron object."""
+
+
+class Scope_of_tests:
+    """Stores the ranges over which the random graphs and SNNs are created for
+    testing purposes."""
+
+    # pylint: disable=R0903
+
+    # pylint: disable=too-many-instance-attributes
+    # Eleven is considered is reasonable in this case.
+    def __init__(self) -> None:
+
+        # Specify graph size (nr. of nodes in graph).
+        self.min_size = 2
+        self.max_size = 30
+
+        # Specify ratio of edges that are created w.r.t. fully connected.
+        self.min_edge_density = 0.2
+        self.max_edge_density = 1
+        self.edge_density_stepsize = 0.1
+
+        # Specify random neuron bias range.
+        self.min_bias = -100
+        self.max_bias = 100
+
+        # Specify random neuron spike threshold voltage range.
+        self.min_vth = 0
+        self.max_vth = 100
+        # Specify random synaptic edge weight range.
+        self.min_edge_weight = -1200
+        self.max_edge_weight = 1200
+
+        # Specify random seed.
+        self.seed = 42
---
+++ tests/test_rand_network_propagation.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""Performs tests that verify lava simulation produces the same results as the
+networkx simulation."""
+
+import math
+import unittest
+
+import networkx as nx
+import numpy as np
+
+from src.get_graph import gnp_random_connected_graph
+from src.Scope_of_tests import Scope_of_tests
+from src.verify_graph_is_snn import (
+    assert_no_duplicate_edges_exist,
+    assert_synaptic_edgeweight_type_is_correct,
+    verify_networkx_snn_spec,
+)
+
+
+class Test_networkx_and_lava_snn_simulation_produce_identical_results(
+    unittest.TestCase
+):
+    """Performs tests that verify lava simulation produces the same results as
+    the networkx simulation."""
+
+    # Initialize test object
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_scope = Scope_of_tests()
+
+    def test_random_networks_are_propagated_the_same_on_networkx_and_lava(
+        self,
+    ):
+        """Tests whether the random_snn_networks that are generated yield the
+        same behaviour on lava as on networkx."""
+        # Get graph without edge to self.
+        for size in range(self.test_scope.min_size, self.test_scope.max_size):
+            for density in np.arange(
+                self.test_scope.min_edge_density,
+                self.test_scope.max_edge_density,
+                self.test_scope.edge_density_stepsize,
+            ):
+
+                # Only generate graphs that have at least 1 edge.
+                if math.floor(size * density) > 1:
+                    G = gnp_random_connected_graph(
+                        density,
+                        size,
+                        self.test_scope,
+                    )
+
+                    # Assert graph is connected.
+                    self.assertTrue(nx.is_connected(G))
+
+                    # Assert size of graph.
+                    self.assertEqual(size, len(G))
+
+                    # Assert number of edges without recurrent edges.
+                    print(f"size={size},density={density}")
+                    # self.assertGreaterEqual(G.number_of_edges(),math.floor(size*density))
+                    # self.assertLessEqual(G.number_of_edges(),math.ceil(size*density))
+
+                    # Assert each edge has a weight.
+                    for edge in G.edges:
+                        assert_synaptic_edgeweight_type_is_correct(G, edge)
+
+                    # Assert no duplicate edges exist.
+                    assert_no_duplicate_edges_exist(G)
+
+                    # Assert all neuron properties are specified.
+                    verify_networkx_snn_spec(G)
+
+                    # Run the simulation on networkx.
+
+                    # Run the simulation on lava.
+
+                    # Verify the simulations produce identical neuron
+                    # properties.
