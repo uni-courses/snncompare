b7d285d2e38a86a5ae26c06b1a23d15aae880fb3
name <some@email.com>
2022-07-05 17:55:51 +0200

    Improved function name and comments of m_vals tests.

--- tests/experiment_settings/test_supported_settings_m_vals.py
+++ tests/experiment_settings/test_supported_settings_m_vals.py
@@ -35,44 +35,88 @@ class Test_m_vals_settings(unittest.TestCase):
         self.rad_sets = rad_sets
         self.with_adaptation_with_radiation = with_adaptation_with_radiation
 
-    def test_m_vals_is_none(self):
-        """Verifies an error is thrown if configuration settings do not contain
-        this setting.."""
-
-        with self.assertRaises(Exception) as context:
-
-            # Configuration Settings of type None throw error.
-            verify_configuration_settings(
-                self.supp_sets, None, has_unique_id=False
-            )
-
-        self.assertEqual(
-            "Error, the experiment_config is of type:"
-            + f"{type(None)}, yet it was expected to be of"
-            + " type dict.",
-            str(context.exception),
+    def test_returns_valid_m_vals(self):
+        """Verifies a valid m is returned."""
+        returned_dict = verify_configuration_settings(
+            self.supp_sets,
+            self.with_adaptation_with_radiation,
+            has_unique_id=False,
         )
+        self.assertIsInstance(returned_dict, dict)
 
-    def test_catch_invalid_m_vals_type(self):
-        """."""
+    def test_error_is_thrown_if_m_vals_key_is_missing(self):
+        """Verifies an exception is thrown if the m_vals key is missing from
+        the MDSA algorithm settings dictionary of the supported algorithms
+        dictionary of the configuration settings dictionary."""
 
-        with self.assertRaises(Exception) as context:
-            # m dictionary of type None throws error.
-            verify_configuration_settings(
-                self.supp_sets, "string_instead_of_dict", has_unique_id=False
-            )
-
-        self.assertEqual(
-            "Error, the experiment_config is of type:"
-            + f'{type("")}, yet it was expected to be of'
-            + " type dict.",
-            str(context.exception),
-        )
-
-    def test_catch_invalid_m_vals_value_type_too_low(self):
-        """."""
         # Create deepcopy of configuration settings.
         config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
+
+        # Remove key and value of m.
+        config_settings["algorithms"]["MDSA"].pop("m_vals")
+
+        with self.assertRaises(Exception) as context:
+            verify_configuration_settings(
+                self.supp_sets, config_settings, has_unique_id=False
+            )
+
+        self.assertEqual(
+            "'m_vals'",
+            str(context.exception),
+        )
+
+    def test_error_is_thrown_for_invalid_m_vals_value_type(self):
+        """Verifies an exception is thrown if the m_vals dictionary value, is
+        of invalid type.
+
+        (Invalid types None, and string are tested, a list with floats
+        is expected).
+        """
+
+        # Create deepcopy of configuration settings.
+        config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
+        expected_type = type(self.supp_sets.algorithms["MDSA"].m_vals)
+
+        # Verify it throws an error on None and string.
+        for invalid_config_setting_value in [None, ""]:
+            config_settings["algorithms"]["MDSA"][
+                "m_vals"
+            ] = invalid_config_setting_value
+            verify_error_is_thrown_on_invalid_configuration_setting_value(
+                invalid_config_setting_value,
+                config_settings,
+                expected_type,
+                self,
+            )
+
+    def test_catch_empty_m_vals_value_list(self):
+        """Verifies an exception is thrown if the m_vals dictionary value is a
+        list without elements."""
+
+        # Create deepcopy of configuration settings.
+        config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
+
+        # Set negative value of m in copy.
+        config_settings["algorithms"]["MDSA"]["m_vals"] = []
+
+        with self.assertRaises(Exception) as context:
+            verify_configuration_settings(
+                self.supp_sets, config_settings, has_unique_id=False
+            )
+
+        self.assertEqual(
+            "Error, list was expected contain at least 1 integer."
+            + f" Instead, it has length:{0}",
+            str(context.exception),
+        )
+
+    def test_catch_invalid_m_vals_value_too_low(self):
+        """Verifies an exception is thrown if the m_vals dictionary value is
+        lower than the supported range of m_vals values permits."""
+
+        # Create deepcopy of configuration settings.
+        config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
+
         # Set negative value of m in copy.
         config_settings["algorithms"]["MDSA"]["m_vals"] = [-2]
 
@@ -91,10 +135,13 @@ class Test_m_vals_settings(unittest.TestCase):
             str(context.exception),
         )
 
-    def test_catch_invalid_m_vals_value_type_too_high(self):
-        """."""
+    def test_catch_invalid_m_vals_value_too_high(self):
+        """Verifies an exception is thrown if the m_vals dictionary value is
+        higher than the supported range of m_vals values permits."""
+
         # Create deepcopy of configuration settings.
         config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
+
         # Set negative value of m in copy.
         config_settings["algorithms"]["MDSA"]["m_vals"] = [50]
 
@@ -112,71 +159,3 @@ class Test_m_vals_settings(unittest.TestCase):
             + f" Instead, it contains:{50}.",
             str(context.exception),
         )
-
-    def test_catch_empty_m_vals_value_list(self):
-        """."""
-        # Create deepcopy of configuration settings.
-        config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
-        # Set negative value of m in copy.
-        config_settings["algorithms"]["MDSA"]["m_vals"] = []
-
-        with self.assertRaises(Exception) as context:
-            verify_configuration_settings(
-                self.supp_sets, config_settings, has_unique_id=False
-            )
-
-        self.assertEqual(
-            "Error, list was expected contain at least 1 integer."
-            + f" Instead, it has length:{0}",
-            str(context.exception),
-        )
-
-    def test_returns_valid_m_vals(self):
-        """Verifies a valid m is returned."""
-        returned_dict = verify_configuration_settings(
-            self.supp_sets,
-            self.with_adaptation_with_radiation,
-            has_unique_id=False,
-        )
-        self.assertIsInstance(returned_dict, dict)
-
-    def test_empty_m_vals(self):
-        """Verifies an exception is thrown if an empty m dict is thrown."""
-
-        # Create deepcopy of configuration settings.
-        config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
-        # Remove key and value of m.
-
-        config_settings["algorithms"]["MDSA"].pop("m_vals")
-
-        with self.assertRaises(Exception) as context:
-            verify_configuration_settings(
-                self.supp_sets, config_settings, has_unique_id=False
-            )
-
-        self.assertEqual(
-            "'m_vals'",
-            str(context.exception),
-        )
-
-    def test_m_vals_value_is_invalid_type(self):
-        """Verifies an exception is thrown if the configuration setting:
-
-        m_vals is of invalid type.
-        """
-
-        # Create deepcopy of configuration settings.
-        config_settings = copy.deepcopy(self.with_adaptation_with_radiation)
-        expected_type = type(self.supp_sets.algorithms["MDSA"].m_vals)
-
-        # Verify it throws an error on None and string.
-        for invalid_config_setting_value in [None, ""]:
-            config_settings["algorithms"]["MDSA"][
-                "m_vals"
-            ] = invalid_config_setting_value
-            verify_error_is_thrown_on_invalid_configuration_setting_value(
-                invalid_config_setting_value,
-                config_settings,
-                expected_type,
-                self,
-            )
