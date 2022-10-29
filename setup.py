61439d7753b34e789ff925314c636e8f09c877d8
Your Name <you@example.com>
2022-10-29 17:06:18 +0200

    Updated setup.cfg.

--- setup.py
+++ setup.py
@@ -1,32 +1,11 @@
-"""This file is to allow this repository to be published as a pip module, such
-that people can install it with: `pip install networkx-to-lava-nc`.
+"""Packaging logic for snnalgocompare."""
+from __future__ import annotations
 
-You can ignore it.
-"""
+import os
+import sys
+
 import setuptools
 
-with open("README.md", encoding="utf-8") as fh:
-    long_description = fh.read()
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
-setuptools.setup(
-    name="snnalgocompare",
-    version="0.0.3",
-    author="a-t-0",
-    author_email="author@example.com",
-    description="Runs an SNN algorithm and compares its results to its Neumann"
-    + "implementation. Also supports adding adaptation and radiation to the "
-    + "SNN.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/a-t-0/networkx-to-lava-nc",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Natural Language :: English",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
-)
+setuptools.setup()
