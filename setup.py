cf911b1e669f8df385be6001a3423f7e8baf60d5
Your Name <you@example.com>
2022-10-29 18:18:52 +0200

    Tried including .tar.gz dependency in pip.

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ import setuptools
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
+install_requires = ["lava @ https://github.com/a-t-0/lava/archive/refs/tags/v0.5.1.tar.gz",]
 setuptools.setup()
