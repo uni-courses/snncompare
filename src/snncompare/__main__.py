8b60a8548847c2b3e8d35228ce160754e3efb740
Your Name <you@example.com>
2022-11-09 13:13:42 +0100

    Started process of restoring mdsa for m>1.

--- src/snncompare/__main__.py
+++ src/snncompare/__main__.py
@@ -9,6 +9,7 @@ from snnalgorithms.get_alg_configs import get_algo_configs, verify_algo_configs
 from snnalgorithms.sparse.MDSA.alg_params import MDSA
 from snnbackends.plot_graphs import create_root_dir_if_not_exists
 
+from .arg_parser.arg_parser import parse_cli_args
 from .exp_setts.default_setts.create_default_settings import (
     create_default_graph_json,
     default_experiment_config,
@@ -21,6 +22,9 @@ if os.path.exists("results"):
 if os.path.exists("latex"):
     shutil.rmtree("latex")
     create_root_dir_if_not_exists("latex/Images/graphs")
+
+# Parse command line interface arguments to determine what this script does.
+args = parse_cli_args()
 
 mdsa = MDSA(list(range(0, 4, 1)))
 mdsa_configs = get_algo_configs(mdsa.__dict__)
@@ -36,6 +40,3 @@ Experiment_runner(
     experiment_config, export_images=export_images, show_snns=show_snns
 )
 # TODO: verify expected output results have been generated successfully.
-
-# Parse command line interface arguments to determine what this script does.
-# args = parse_cli_args()
