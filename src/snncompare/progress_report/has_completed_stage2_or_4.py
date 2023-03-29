4e924790834a96fa3cc89fe55bd1e0fe644aae3a
name <example@example.com>
2023-03-29 14:05:09 +0200

    Removed debugging artifact.

--- src/snncompare/progress_report/has_completed_stage2_or_4.py
+++ src/snncompare/progress_report/has_completed_stage2_or_4.py
@@ -104,8 +104,7 @@ def has_outputted_radiation_json(
         snn_graph: Union[
             nx.DiGraph, Simulator
         ] = get_snn_graph_from_graphs_dict(
-            # with_adaptation=with_adaptation,
-            with_adaptation=True,
+            with_adaptation=with_adaptation,
             with_radiation=False,
             graphs_dict=graphs_dict,
         )
@@ -121,5 +120,6 @@ def has_outputted_radiation_json(
             )
         )
         if not radiation_data.radiation_file_exists:
+            print(f"FALSE:{radiation_data.radiation_filepath}")
             return False
     return True
