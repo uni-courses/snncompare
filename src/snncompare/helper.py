d24fe0ebdfb37b06157dd05c2ee52f42216ad084
name <example@example.com>
2023-04-04 15:57:44 +0200

    Rewrote neumann algorithm.

--- src/snncompare/helper.py
+++ src/snncompare/helper.py
@@ -5,12 +5,9 @@ from pathlib import Path
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Union
 
 import networkx as nx
-import pylab as plt
 from networkx.classes.graph import Graph
 from simsnn.core.simulators import Simulator
 from typeguard import typechecked
-
-from snncompare.export_plots.plot_graphs import export_plot
 
 if TYPE_CHECKING:
     pass
@@ -49,105 +46,6 @@ def generate_list_of_n_random_nrs(
     )
 
 
-@typechecked
-def compute_mark(*, input_graph: nx.Graph, rand_ceil: float) -> None:
-    """Computes the mark at the counter neurons after the simulation is
-    completed.
-
-    TODO: move into algorithms module.
-    """
-    # Compute the mark based on degree+randomness=weight
-    for node in input_graph.nodes:
-        max_weight = max(
-            input_graph.nodes[n]["weight"]
-            for n in nx.all_neighbors(input_graph, node)
-        )
-
-        nr_of_max_weights = 0
-        for n in nx.all_neighbors(input_graph, node):
-            if (
-                input_graph.nodes[n]["weight"] == max_weight
-            ):  # Should all max weight neurons be marked or only one of them?
-                # Read of the score from countermarks, not marks.
-                input_graph.nodes[n]["marks"] += rand_ceil + 1
-                input_graph.nodes[n]["countermarks"] += 1
-                nr_of_max_weights = nr_of_max_weights + 1
-                # Verify there is only one max weight neuron.
-                if nr_of_max_weights > 1:
-                    raise ValueError("Two numbers with identical max weight.")
-
-
-@typechecked
-def plot_alipour(
-    *,
-    configuration: str,
-    seed: int,
-    size: int,
-    m: int,
-    G: nx.DiGraph,
-    export: bool = True,
-    show: bool = False,
-) -> None:
-    """
-
-    :param configuration:
-    param iteration: The initialisation iteration that is used.
-    :param seed: The value of the random seed used for this test.
-    :param size: Nr of nodes in the original graph on which test is ran.
-    :param m: The amount of approximation iterations used in the MDSA
-    approximation.
-    :param G: The original graph on which the MDSA algorithm is ran.
-    :param export:  (Default value = True)
-    :param show:  (Default value = False)
-
-    """
-    # pylint: disable=R0913
-    # TODO: reduce 8/5 input arguments to at most 5/5.
-    the_labels = get_alipour_labels(G=G, configuration=configuration)
-    # nx.draw_networkx_labels(G, pos=None, labels=the_labels)
-    npos = nx.circular_layout(
-        G,
-        scale=1,
-    )
-    nx.draw(G, npos, labels=the_labels, with_labels=True)
-    if show:
-        plt.show()
-    if export:
-        export_plot(
-            plt,
-            f"alipour_{seed}_size{size}_m{m}_iter_combined_"
-            + f"{configuration}",
-            extensions=["png"],  # TODO: include run_config extensions.
-        )
-
-    plt.clf()
-    plt.close()
-
-
-@typechecked
-def get_alipour_labels(*, G: nx.DiGraph, configuration: str) -> Dict[str, str]:
-    """
-
-    :param G: The original graph on which the MDSA algorithm is ran.
-    :param configuration:
-
-    """
-    labels = {}
-    for node_name in G.nodes:
-        if configuration == "0rand_mark":
-            labels[node_name] = (
-                f'{node_name},R:{G.nodes[node_name]["random_number"]}, M:'
-                + f'{G.nodes[node_name]["marks"]}'
-            )
-        elif configuration == "1weight":
-            labels[
-                node_name
-            ] = f'{node_name}, W:{G.nodes[node_name]["weight"]}'
-        else:
-            raise NotImplementedError("Unsupported configuration.")
-    return labels
-
-
 # checks if file exists
 @typechecked
 def file_exists(*, filepath: str) -> bool:
@@ -160,97 +58,6 @@ def file_exists(*, filepath: str) -> bool:
     # function.
     my_file = Path(filepath)
     return my_file.is_file()
-
-
-@typechecked
-def compute_marks_for_m_larger_than_one(
-    *,
-    input_graph: nx.Graph,
-    m: int,
-    seed: int,
-    size: int,
-    rand_ceil: int,
-    export: bool = False,
-    show: bool = False,
-) -> None:
-    """Stores the marks in the counter nodes of the graph.."""
-    # pylint: disable=R0913
-    # TODO: reduce 10/5 arguments to at most 5/5.
-    # Don't compute for m=0
-    for loop in range(1, m + 1):
-        for node in input_graph.nodes:
-            # Compute the weights for this round of m.
-            input_graph.nodes[node]["weight"] = (
-                input_graph.nodes[node]["marks"]
-                + input_graph.nodes[node]["random_number"]
-            )
-
-            # Reset marks.
-            input_graph.nodes[node]["marks"] = 0
-            input_graph.nodes[node]["countermarks"] = 0
-
-        for node in input_graph.nodes:
-            max_weight = max(
-                input_graph.nodes[n]["weight"]
-                for n in nx.all_neighbors(input_graph, node)
-            )
-            for n in nx.all_neighbors(input_graph, node):
-                if input_graph.nodes[n]["weight"] == max_weight:
-                    # Always raise mark always by (rand_ceil + 1) * delta
-                    # (not by 1).
-                    input_graph.nodes[n]["marks"] += rand_ceil + 1
-                    input_graph.nodes[n]["countermarks"] += 1
-        if show or export:
-            plot_alipour(
-                configuration="0rand_mark",
-                seed=seed,
-                size=size,
-                m=loop,
-                G=input_graph,
-                show=show,
-            )
-            plot_alipour(
-                configuration="1weight",
-                seed=seed,
-                size=size,
-                m=loop,
-                G=input_graph,
-                show=show,
-            )
-            plot_alipour(
-                configuration="2inhib_weight",
-                seed=seed,
-                size=size,
-                m=loop,
-                G=input_graph,
-                show=show,
-            )
-
-
-@typechecked
-def set_node_default_values(
-    *,
-    input_graph: nx.Graph,
-    node: int,
-    rand_ceil: float,
-    uninhibited_spread_rand_nrs: List[float],
-) -> None:
-    """Initialises the starting values of the node attributes."""
-    # pylint: disable=R0913
-    # TODO: reduce 6/5 arguments to at most 5/5.
-    # Initialise values.
-    # G.nodes[node]["marks"] = 0
-    input_graph.nodes[node]["marks"] = input_graph.degree(node) * (
-        rand_ceil + 1
-    )
-    input_graph.nodes[node]["countermarks"] = 0
-    input_graph.nodes[node]["random_number"] = (
-        1 * uninhibited_spread_rand_nrs[node]
-    )
-    input_graph.nodes[node]["weight"] = (
-        input_graph.degree(node) * (rand_ceil + 1)
-        + input_graph.nodes[node]["random_number"]
-    )
 
 
 @typechecked
