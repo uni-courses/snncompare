58f4659d55d67eb1c03ac55c8cc01d556d07d3f4
name <you@example.com>
2023-06-07 17:57:07 +0200

    Included list of output data comments.

---
+++ src/snncompare/output_settings/exp_config_to_tex.py
@@ -0,0 +1,30 @@
+"""Outputs data on the experiment config to files importable by latex."""
+# Chapter: Abstract, minimum amplitude of synapse excitation PER RUN.
+# Chapter: Abstract, max amplitude of synapse excitation PER RUN.
+
+# Chapter: Abstract, minimum probability of synapse excitation occurrence, per
+# synapse, per timestep. 
+# Chapter: Abstract, maximum probability of synapse excitation occurrence, per 
+# synapse, per timestep.
+
+# Chapter: Abstract, minimum probability of neuron death occurrence, per 
+# neuron, per timestep. 
+# Chapter: Abstract, maximum probability of neuron death occurrence, per 
+# neuron, per timestep.
+
+
+# Chapter: Methodology, number of synaptic excitation events per simulation.
+# Chapter: Methodology, the average synaptic weight increase in percentages, per simulation.
+
+# Chapter: Methodology, maximum neuron death probability per simulation in this experiment config.
+
+# Chapter: Methodology, all neuron death probabilities per simulation.
+
+# Chapter: Methodology: used m_values in experiment.
+# Chapter: Methodology: list of used pseudo random seeds in experiment.
+
+# Chapter: Methodology: Table with Experiment run config settings.
+
+# Chapter: Methodology: The number of scores per adaptation type, per radiation setting.
+
+# TODO: Chapter: Results: The number of boolean pass/fails per adapted snn per dot in the boxplot.
